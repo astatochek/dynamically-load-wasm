3c7ed928c2f6f031c1dff79b11bf589483aaa1fa
astatochek <astatochek@gmail.com>
2025-03-30 11:56:06 +0300

    - cleanup

--- web-ui/src/app/app.component.ts
+++ web-ui/src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: ``,
 })
-export class AppComponent {
-  title = 'web-ui';
-}
+export class AppComponent {}
