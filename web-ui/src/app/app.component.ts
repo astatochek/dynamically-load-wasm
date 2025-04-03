4898f7c4b4a05815b46e7115e94a6b52cfe1ef80
astatochek <astatochek@gmail.com>
2025-04-03 22:16:47 +0300

    - add kanagawa theme

--- web-ui/src/app/app.component.ts
+++ web-ui/src/app/app.component.ts
@@ -3,60 +3,64 @@ import { FactorialService } from './factorial.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   template: `
-    <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <main class="min-h-screen flex items-center justify-center bg-sumiInk1">
       <div
-        class="bg-white rounded-lg shadow-xl p-8 w-full max-w-md border-2 border-purple-300"
+        class="w-full max-w-md p-8 rounded-lg shadow-xl bg-sumiInk0 border-2 border-oniViolet"
       >
-        <h2 class="text-2xl font-semibold text-center mb-6 text-purple-600">
+        <h1 class="text-3xl font-bold text-center mb-6 text-oldWhite">
           Factorial Calculator
-        </h2>
-
-        <div class="mb-4">
-          <label
-            for="factorial-input"
-            class="block text-sm font-medium text-gray-700"
-          >
-            Enter a Number (0-20):
-          </label>
-          <input
-            type="number"
-            id="factorial-input"
-            #input
-            value="5"
-            min="0"
-            max="20"
-            (keyup.enter)="num.set(input.valueAsNumber)"
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-50 text-xl p-2"
-          />
-        </div>
-
-        <button
-          (click)="num.set(input.valueAsNumber)"
-          class="w-full bg-purple-500 transition duration-400 ease-in-out hover:bg-purple-600 text-white font-bold py-3 px-4 rounded-md focus:outline-none focus:shadow-outline"
+        </h1>
+        <form
+          (submit)="$event.preventDefault(); num.set(input.valueAsNumber)"
+          class="space-y-6"
         >
-          Calculate
-        </button>
+          <div class="space-y-2">
+            <label
+              for="factorial-input"
+              class="block text-fujiWhite font-medium"
+            >
+              Enter a number:
+            </label>
+            <input
+              type="number"
+              id="factorial-input"
+              #input
+              value="5"
+              min="1"
+              max="20"
+              class="w-full px-4 py-2 rounded-md bg-sumiInk2 text-oldWhite border border-sumiInk4 focus:outline-none focus:ring-2 focus:ring-crystalBlue"
+              placeholder="Enter a number"
+            />
+          </div>
+
+          <button
+            type="submit"
+            class="w-full py-2 px-4 bg-waveAqua2 hover:bg-springGreen transition-colors duration-300 text-sumiInk1 font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-crystalBlue"
+          >
+            Calculate
+          </button>
+        </form>
 
         @if (query.error()) {
-          <div
-            class="mt-4 text-red-600 border border-red-300 rounded-md p-3 bg-red-50"
-          >
-            {{ query.error() }}
-          </div>
-        } @else if (query.value()) {
-          <div
-            class="mt-4 text-green-600 border border-green-300 rounded-md p-3 bg-green-50"
-          >
-            factorial({{ num() }}) = <strong>{{ query.value() }}</strong>
+          <div class="mt-6 p-4 rounded-md bg-winterRed border border-lotusRed2">
+            <h2 class="text-lg font-medium text-fujiWhite">Error:</h2>
+            <p class="text-2xl font-bold text-lotusRed3">{{ query.error() }}</p>
           </div>
         } @else {
-          <div class="mt-4 text-slate-500 rounded-md p-3">calculating...</div>
+          <div
+            class="mt-6 p-4 rounded-md bg-winterGreen border border-autumnGreen"
+          >
+            <h2 class="text-lg font-medium text-fujiWhite">Result:</h2>
+            <p class="text-2xl font-bold text-springGreen">
+              {{ query.value() }}
+            </p>
+          </div>
         }
       </div>
-    </div>
+    </main>
   `,
-  standalone: true,
 })
 export class AppComponent {
   num = signal(5);
@@ -67,9 +71,15 @@ export class AppComponent {
     request: () => this.num(),
     loader: ({ request: num }) => {
       if (isNaN(num)) {
-        return Promise.resolve(void 0);
+        return Promise.resolve('...');
       }
-      return this.factorial.calculate(num);
+      return this.factorial.calculate(num).then(String);
     },
+    defaultValue: '...',
   });
+
+  onSibmit(event: SubmitEvent, value: number) {
+    event.preventDefault();
+    this.num.set(value);
+  }
 }
